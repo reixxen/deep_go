a5e9bbe37c99ddecb06399a9fdd36e8dc9c9b3df
reixxen <ashesdonotburn@vk.com>
2026-01-16 01:18:16 +0500

    homework: data_types

--- homework/data_types/homework_test.go
+++ homework/data_types/homework_test.go
@@ -1,15 +1,56 @@
 package main
 
 import (
+	"math/bits"
 	"testing"
+	"unsafe"
 
 	"github.com/stretchr/testify/assert"
 )
 
 // go test -v homework_test.go
+// go test -bench=. homework_test.go
 
-func ToLittleEndian(number uint32) uint32 {
-	return 0 // need to implement
+func ToLittleEndian[T uint16 | uint32 | uint64](number T) T {
+	var res T
+	size := int(unsafe.Sizeof(number))
+
+	for i := range size {
+		byte := (number >> (8 * i)) & 0xFF
+		res |= byte << (8 * (size - 1 - i))
+	}
+
+	return res
+}
+
+func ReverseBytes[T uint16 | uint32 | uint64](number T) T {
+	var res T
+	switch unsafe.Sizeof(number) {
+	case 2:
+		res = T(bits.ReverseBytes16(uint16(number)))
+	case 4:
+		res = T(bits.ReverseBytes32(uint32(number)))
+	case 8:
+		res = T(bits.ReverseBytes64(uint64(number)))
+	}
+
+	return res
+}
+
+func BenchmarkToLittleEndian(b *testing.B) {
+	var v = []uint32{0x00000000, 0xFFFFFFFF, 0x00FF00FF, 0x0000FFFF, 0x01020304}
+
+	for i := 0; i < b.N; i++ {
+		_ = ToLittleEndian(v[i%len(v)])
+	}
+}
+
+func BenchmarkReverseBytes(b *testing.B) {
+	var v = []uint32{0x00000000, 0xFFFFFFFF, 0x00FF00FF, 0x0000FFFF, 0x01020304}
+
+	for i := 0; i < b.N; i++ {
+		_ = ReverseBytes(v[i%len(v)])
+	}
 }
 
 func TestСonversion(t *testing.T) {
